4fb4abec926a7c773cf16dc5be95228e7c3809d3
IvChang <ivan2chang@gmail.com>
2024-07-12 15:24:17 -0400

    Fait: Affiche les infos principaux de la journée courante (codé dure)

--- src/Containers/Aujourdhui.jsx
+++ src/Containers/Aujourdhui.jsx
@@ -1,6 +1,37 @@
+import "./Aujourdhui.css";
+
 
 export default function Aujourdhui () {
     return (
-        <h1>Aujourdhui</h1>
+        <>
+            <div className="blocPrincipal">
+                <div className="ruban">
+                    <p>Laval, QC</p>
+                    <p>3:10 pm</p>
+                </div>
+                
+                <h2>Nuageux</h2>
+                <div className="tempCourant">
+                    <h1>30°</h1>
+                    <p>C</p>
+                </div>
+                
+                <div className="sentComme">
+                    <p>Sent comme</p>
+                    <h3>33°</h3>
+                    <p>C</p>
+                </div>
+                
+                
+
+                
+            </div>
+
+            <div className="blocSecondaire">
+                <p>Vent</p>
+                <h2>NE 14 km/h</h2>
+                <hr></hr>
+            </div>
+        </>
     )
 };
