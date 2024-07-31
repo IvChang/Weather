75f7ce858bf8eb99e16f1018e65ee89a9d33c194
IvChang <ivan2chang@gmail.com>
2024-07-31 11:48:51 -0400

    Fixed: Assure bonne position de la barre selon taille de fenêtre et l'élément de navigation cliqué

--- src/Components/navigation/Liens.jsx
+++ src/Components/navigation/Liens.jsx
@@ -1,16 +1,20 @@
 import "./Liens.css";
 import { NavLink } from "react-router-dom";
+import { useState } from 'react';
 
 export default function Liens() {
+    const [barreActive, setBarreActive] = useState("animation bar-aujourdhui");
+
     return (
         <ul className="liens">
             <li>
-                <NavLink to="/Aujourdhui">Aujourd'hui</NavLink>
+                <NavLink to="/Aujourdhui" onClick={() =>setBarreActive("animation bar-aujourdhui")}>Aujourd'hui</NavLink>
             </li>
             <li>
-                <NavLink to="/Quotidien">Quotidien</NavLink>
+                <NavLink to="/Quotidien" onClick={() =>setBarreActive("animation bar-quotidien")}>Quotidien</NavLink>
             </li>
-            <div className="animation bar-aujourdhui"></div>
+            <div className={barreActive}></div>
+            
             
         </ul>
     )
